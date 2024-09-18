0210b78e49315fd7867f823cc0ae71ce133cbe4b
krishhh16 <krishh16@yahoo.com>
2024-09-18 20:01:58 +0530

    added try button

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export const runtime = "edge";
 
 export default function Home() {
   const [isReady, setIsReady] = useState(false);
-  const [action, setAction] = useState <Action | null> (null);
+  const [action, setAction] = useState<Action | null>(null);
   const [action2, setAction2] = useState<Action>();
   const [action3, setAction3] = useState<Action>();
   const [action4, setAction4] = useState<Action>();
@@ -40,24 +40,24 @@ export default function Home() {
     }
   };
 
-  const actionApiUrl  = `https://dial.to/?action=solana-action%3A${encodeURIComponent(window.location.origin)}%2Fapi%2Fapp%2Fsponsor%2Faction%3Fid%3Dweb2&cluster=devnet`; // H
+  const actionApiUrl = `https://dial.to/?action=solana-action%3A${encodeURIComponent(window.location.origin)}%2Fapi%2Fapp%2Fsponsor%2Faction%3Fid%3Dweb2&cluster=devnet`; // H
   const actionApiUrl2 = 'https://dial.to/?action=solana-action:https://tensor.dial.to/buy-floor/madlads';
   const actionApiUrl3 = `https://dial.to/?action=solana-action%3A${encodeURIComponent(window.location.origin)}%2Fapi%2Fapp%2Fsponsor%2Faction%3Fid%3Dml&cluster=devnet`;
-  const actionApiUrl4= 'https://dial.to/?action=solana-action:https://hermans.club/api/actions/presale/phasetwo';
+  const actionApiUrl4 = 'https://dial.to/?action=solana-action:https://hermans.club/api/actions/presale/phasetwo';
   const actionApiUrl5 = `https://dial.to/?action=solana-action%3A${encodeURIComponent(window.location.origin)}%2Fapi%2Fapp%2Fsponsor%2Faction%3Fid%3Dweb3&cluster=devnet`;
   const actionApiUrl6 = 'https://dial.to/?action=solana-action:https://100xdevsblink.me/api/actions/payments';
   const actionApiUrl7 = `https://dial.to/?action=solana-action%3A${encodeURIComponent(window.location.origin)}%2Fapi%2Fapp%2Factions%3Fid%3D6a6548a0-fb3a-4217-9608-71fbe9250056&cluster=devnet`;
-  
+
   // useAction initiates registry, adapter and fetches the action.
   console.log("I ran till this>")
   const { adapter } = useActionSolanaWalletAdapter('https://api.devnet.solana.com');
   const { action: something } = useAction({ url: actionApiUrl, adapter });
-  const { action: something2 } = useAction({ url:  actionApiUrl2, adapter });
+  const { action: something2 } = useAction({ url: actionApiUrl2, adapter });
   const { action: something3 } = useAction({ url: actionApiUrl3, adapter });
-  const { action: something4 } = useAction({ url:  actionApiUrl4, adapter });
-  const { action: something5 } = useAction({ url:  actionApiUrl5, adapter });
-  const { action: something6 } = useAction({ url:  actionApiUrl6, adapter });
-  const { action: something7 } = useAction({ url:  actionApiUrl7, adapter });
+  const { action: something4 } = useAction({ url: actionApiUrl4, adapter });
+  const { action: something5 } = useAction({ url: actionApiUrl5, adapter });
+  const { action: something6 } = useAction({ url: actionApiUrl6, adapter });
+  const { action: something7 } = useAction({ url: actionApiUrl7, adapter });
   useEffect(() => {
     let scroll: LocomotiveScroll | undefined;
     import("locomotive-scroll").then((locomotiveModule) => {
@@ -81,45 +81,45 @@ export default function Home() {
     setAction7(something7 as Action);
 
   }, [something, something7, something2, something3, something4, something5, something6])
-  
-  
+
+
   if (!isReady) {
     return null;
   }
 
   return (
-    <div  
+    <div
       style={{ paddingTop: "0.1px", paddingBottom: "0.1px" }}
       className="bg-zinc-900 overflow-x-hidden"
     >
-      <Navbar scrollToMarquees={scrollToMarquees} scrollToFeatures={scrollToFeatures} />
-      <Work />
-      <Stripes />
-      <div ref={marqueeRef} className="relative hidden md:flex h-[100vh] w-full py-10 items-center justify-center overflow-hidden mt-[-40px] mb-[-250px]">
-       { (action && action2 && action3 && action4 && action5 && action6 &&  action7 ) && 
-        <Marquee>
-          {
-            [action, action2, action3, action4, action5, action6, action7].map((item, index) => (
-              <div
-                className="flex h-[20vh] w-[25vw] flex-1 flex-col justify-between rounded-md"
-                key={`item-${index}`}
-              >
-                <Blink stylePreset="x-dark" action={item} />
-              </div>
-            ))
-          }
-        </Marquee>
-      }
-      </div>
-      <div id="trialBounty">
-        <TrialBounty />
-      </div>
-      {/* //@ts-ignore */}
-      <div className="mt-10 lg:mt-40 xl:mt-20" ref={featureRef}>
-       <Products />
-      </div>
-      <Cards />
-      <Footer />
-    </div>
-  );
+          <Navbar scrollToMarquees={scrollToMarquees} scrollToFeatures={scrollToFeatures} />
+          <Work />
+          <Stripes />
+          <div ref={marqueeRef} className="relative hidden md:flex h-[100vh] w-full py-10 items-center justify-center overflow-hidden mt-[-40px] mb-[-250px]">
+            {(action && action2 && action3 && action4 && action5 && action6 && action7) &&
+              <Marquee>
+                {
+                  [action, action2, action3, action4, action5, action6, action7].map((item, index) => (
+                    <div
+                      className="flex h-[20vh] w-[25vw] flex-1 flex-col justify-between rounded-md"
+                      key={`item-${index}`}
+                    >
+                      <Blink stylePreset="x-dark" action={item} />
+                    </div>
+                  ))
+                }
+              </Marquee>
+            }
+          </div>
+          <div id="trialBounty">
+            <TrialBounty />
+          </div>
+          {/* //@ts-ignore */}
+          <div className="mt-10 lg:mt-40 xl:mt-20" ref={featureRef}>
+            <Products />
+          </div>
+          <Cards />
+          <Footer />
+        </div>
+          );
 }
